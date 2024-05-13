327405b52ad3648c1653152c4185951a479e3b94
Mohak-Trivedi <trivedimohak@gmail.com>
2024-05-13 22:44:50 +0530

    o/p based map filter reduce questions

--- 2-map-filter-reduce/script.js
+++ 2-map-filter-reduce/script.js
@@ -145,3 +145,62 @@
 // }, 0);
 
 // console.log(sum);
+
+
+/////////////////////////////////////////////////////
+
+// o/p based questions:
+
+let students = [
+  { name: "Piyush", rollNumber: 31, marks: 80 },
+  { name: "Jenny", rollNumber: 15, marks: 69 },
+  { name: "Kaushal", rollNumber: 16, marks: 35 },
+  { name: "Dilpreet", rollNumber: 7, marks: 55 },
+];
+
+// Q1 - Return only the names of students in capital
+
+// Using Traditional for-loop:
+// let names = [];
+// for(let obj of students) {
+//     names.push(obj.name.toUpperCase());
+// }
+// console.log(names);
+
+// Using map
+let names = students.map((stu) => stu.name.toUpperCase());
+console.log(names);
+
+
+// Q2 - we want to get the details of students who scored more than 60 marks.
+let gradeAstuds = students.filter((stu) => stu.marks>60);
+console.log(gradeAstuds);
+
+// Q3 - Get the details of students who scored more than 60 marks and have rollNumber greater than 15.
+let details = students.filter((stu) => stu.marks>60 && stu.rollNumber>15);
+console.log(details);
+
+// Q4 - Sum total of the marks of the students
+let total = students.reduce((acc, stu) => {
+    return acc + stu.marks;
+}, 0);
+console.log(total);
+
+// Q5 - Get only the names of the students who scored more than 60 marks
+let gradeAnames = students.filter((stu) => stu.marks > 60).map((stu) => stu.name);
+console.log(gradeAnames);
+
+// Q6 - print the total marks of the students with marks greater than 60 after 20 marks has been added to those students who scored less than 60.
+let grace = students.map((stu) => {
+    if(stu.marks < 60) {
+        stu.marks += 20;
+    }
+
+    return stu;
+})
+.filter((stu) => stu.marks > 60)
+.reduce((acc, stu) => {
+    return acc + stu.marks;
+}, 0);
+
+console.log(grace);
