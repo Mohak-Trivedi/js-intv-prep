780dfe69370e883cb84bbfc82e9c9e794e4c14dc
Mohak-Trivedi <trivedimohak@gmail.com>
2024-05-13 22:14:51 +0530

    created polyfills for map filter reduce

--- 2-map-filter-reduce/script.js
+++ 2-map-filter-reduce/script.js
@@ -60,3 +60,88 @@
 /////////////////////////////////////////////////////
 
 
+// Polyfill of map()
+
+// map syntax for reference: 
+// Array.map((curr, idx, arr) => {});
+
+// Define Polyfill
+// Array.prototype.myMap = function (cb) {
+//     let temp = [];
+
+//     // nums.myMap -> nums = this
+//     for(let i = 0;i < this.length;i++) {
+//         temp.push(cb(this[i], i, this));
+//     }
+
+//     return temp;
+// };
+
+// Test polyfill:
+// const nums = [1, 2, 3, 4];
+
+// const multiplyThree = nums.myMap((num, idx, arr) => {
+//     return num * 3 + idx;
+// });
+
+// console.log(multiplyThree);
+
+
+/////////////////////////////////////////////////////
+
+
+// Polyfill of filter()
+
+// filter syntax for reference:
+// Array.filter((curr, idx, arr) => {});
+
+// Define Polyfill
+// Array.prototype.myFilter = function (cb) {
+//     let temp = [];
+
+//     for(let i = 0;i < this.length;i++) {
+//         if(cb(this[i], i, this)) {
+//             temp.push(this[i]);
+//         }
+//     }
+
+//     return temp;
+// }
+
+// Test polyfill:
+// const nums = [1, 2, 3, 4];
+
+// const moreThanTwo = nums.myFilter((num, idx, arr) => {
+//     return num > 2;
+// });
+
+// console.log(moreThanTwo);
+
+
+/////////////////////////////////////////////////////
+
+
+// Polyfill of reduce()
+
+// reduce syntax for reference:
+// Array.reduce((acc, curr, idx, arr) => {}, initialValue);
+
+// Define Polyfill
+// Array.prototype.myReduce = function (cb, initialValue) {
+//     var accumulator = initialValue;
+
+//     for(let i = 0;i < this.length;i++) {
+//         accumulator = accumulator ? cb(accumulator, this[i], i, this) : this[i];
+//     }
+
+//     return accumulator;
+// };
+
+// Test polyfill:
+// const nums = [1, 2, 3, 4];
+
+// const sum = nums.myReduce((acc, num, idx, arr) => {
+//     return acc + num;
+// }, 0);
+
+// console.log(sum);
