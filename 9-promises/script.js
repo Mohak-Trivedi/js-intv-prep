194ff18aed2841d1d0377548aeb611ff262e50a3
Mohak-Trivedi <trivedimohak@gmail.com>
2024-06-12 19:33:46 +0530

    replace callback hell with Promises and 2 Promise combinators

--- 9-promises/script.js
+++ 9-promises/script.js
@@ -297,3 +297,150 @@
 // Why haven't you subscribed to Mohak Trivedi
 
 // Notice we got rejected directly instead of Pending first and then rejected
+
+
+// Question - How can Promise be used to make our previous code more readable?
+// console.log('start');
+
+// function importantAction(username, cb) {
+//     setTimeout(() => {
+//         cb(`Subscribe to ${username}`);
+//     }, 0);
+// }
+
+// function likeTheVideo(video, cb) {
+//     setTimeout(() => {
+//         cb(`Like the ${video} video`);
+//     }, 1000);
+// }
+
+// function shareTheVideo(video, cb) {
+//     setTimeout(() => {
+//         cb(`Share the ${video} video`);
+//     }, 1000);
+// }
+
+// const message = importantAction('Mohak Trivedi', function (message) {
+//     console.log(message);
+//     likeTheVideo("JS interview questions", (action) => {
+//         console.log(action);
+//         shareTheVideo("JS interview questions", (action) => {
+//             console.log(action);
+//         });
+//     });
+// });
+
+// console.log('stop');
+
+// Answer:
+console.log('start');
+
+function importantAction(username) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve(`Subscribe to ${username}`);
+        }, 1000);
+    });
+}
+
+function likeTheVideo(video) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve(`Like the ${video} video`);
+        }, 1000);
+    });
+}
+
+function shareTheVideo(video) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve(`Share the ${video} video`);
+        }, 1000);
+    });
+}
+
+// importantAction('Mohak Trivedi').then((res) => {
+//     console.log(res);
+//     likeTheVideo("JS interview questions").then((res) => {
+//         console.log(res);
+//         shareTheVideo("JS interview questions").then((res) => {
+//             console.log(res);
+//         });
+//     });
+// }).catch((err) => {
+//     console.error(err);
+// });
+
+// console.log('stop');
+
+
+// Question - Promise Chaining:
+// But, even with Promises in the above e.g. 
+// we're getting a structure similar to callback hell, just that
+// instead of the callbacks we now have Promises i.e. instead of callback inside
+// callback, we are now using .then() inside .then()
+// To solve this issue, we will use Promise chaining.
+// importantAction('Mohak Trivedi').then((res) => {
+//     console.log(res);
+//     return likeTheVideo("JS interview questions");
+// }).then((res) => {
+//     console.log(res);
+//     return shareTheVideo("JS interview questions");
+// }).then((res) => {
+//     console.log(res);
+// }
+// ).catch((err) => {
+//     console.error(err);
+// });
+
+// console.log('stop');
+// As we see, now even though we use all the 3 promises, we are still not needing
+// to nest one .then() inside another because we are using Promise Chaining now.
+
+
+// Question - Promise Combinator
+// But, even in the above code, we see that even though it is not having a 
+// callback hell like structure, it is still very lengthy.
+
+// Promise Combinators help us to execute more than one Promise at a time.
+// There are mainly 4 types of Promise Combinators
+
+// 1. Promise.all():
+// Runs all the Promises provided to it parallely and returns an array of all the
+// fulfilled Promises, but it fails even if a single Promise gets failed.
+// Run once by replacing 'resolve' in likeTheVideos() with 'reject'
+
+// Promise.all([
+//     importantAction('Mohak Trivedi'),
+//     likeTheVideo("JS interview questions"),
+//     shareTheVideo("JS interview questions")
+// ]).then((res) => {
+//     console.log(res);
+// }).catch((err) => {
+//     console.error("Error: Promises Failed.", err);
+// });
+
+// console.log('stop');
+
+// 2. Promise.race():
+// Similar to Promise.all() but it returns the first Promise resolved or rejected.
+// Set the timeout for shareTheVideos() to 500 i.e. lowest among the three, and
+// you'll notice that the value resolved by that Promise gets logged even though
+// it is the last mentioned Promise.
+// Also, next set the timeout for likeTheVideos() to 100 and replace its 
+// 'resolve' with 'reject', you'll now see its error message as race() returns
+// the first Promise resolved or rejected.
+
+Promise.race([
+    importantAction('Mohak Trivedi'),
+    likeTheVideo("JS interview questions"),
+    shareTheVideo("JS interview questions")
+]).then((res) => {
+    console.log(res);
+}).catch((err) => {
+    console.error("Error: Promises Failed.", err);
+});
+
+console.log('stop');
+
+// 3. Promise.allSettled()
