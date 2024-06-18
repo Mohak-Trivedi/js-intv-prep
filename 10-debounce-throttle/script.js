2bef363072c5a6044b9dabfa4818f9932fcb2cf3
Mohak-Trivedi <trivedimohak@gmail.com>
2024-06-18 09:07:21 +0530

    throttle polyfill

--- 10-debounce-throttle/script.js
+++ 10-debounce-throttle/script.js
@@ -46,41 +46,43 @@
 
 
 // Question - Debounce Polyfill
-const btn = document.querySelector('.increment_btn');
-const btnPress = document.querySelector('.increment_pressed');
-const count = document.querySelector('.increment_count');
 
-var pressedCount = 0;
-var triggeredCount = 0;
+// Answer:
+// const btn = document.querySelector('.increment_btn');
+// const btnPress = document.querySelector('.increment_pressed');
+// const count = document.querySelector('.increment_count');
 
-// Method 2: Using Debounce Polyfill
-// Requirements seen from Lodash debounce():
-// - Takes in : a callback and a time (delay)
-// - Returns: a debounced function
-const myDebounce = (cb, delay) => {
-    // Since we need to measure the time after every keystroke or button press,
-    // we need to maintain a timer.
-    let timer;
+// var pressedCount = 0;
+// var triggeredCount = 0;
 
-    // ...args to rest the extra arguments that we pass to myDebouncedCount()
-    // that must also be passed to the callback
-    return function (...args) {
-        if(timer) clearTimeout(timer);
-        
-        timer = setTimeout(() => {
-            cb(...args);
-        }, delay);
-    }
-};
+// // Method 2: Using Debounce Polyfill
+// // Requirements seen from Lodash debounce():
+// // - Takes in : a callback and a time (delay)
+// // - Returns: a debounced function
+// const myDebounce = (cb, delay) => {
+//     // Since we need to measure the time after every keystroke or button press,
+//     // we need to maintain a timer.
+//     let timer;
 
-const myDebouncedCount = myDebounce(() => {
-    count.innerHTML = ++triggeredCount;
-}, 800);
+//     // ...args to rest the extra arguments that we pass to myDebouncedCount()
+//     // that must also be passed to the callback
+//     return function (...args) {
+//         if(timer) clearTimeout(timer);
 
-btn.addEventListener('click', () => {
-    btnPress.innerHTML = ++pressedCount;
-    myDebouncedCount();
-});
+//         timer = setTimeout(() => {
+//             cb(...args);
+//         }, delay);
+//     }
+// };
+
+// const myDebouncedCount = myDebounce(() => {
+//     count.innerHTML = ++triggeredCount;
+// }, 800);
+
+// btn.addEventListener('click', () => {
+//     btnPress.innerHTML = ++pressedCount;
+//     myDebouncedCount();
+// });
 
 // What happens if we don't mention:
 // if(timer) clearTimeout(timer);
@@ -90,3 +92,43 @@ btn.addEventListener('click', () => {
 // 3rd click, the callback is executed 3 times i.e. for all the 3 clicks.
 // This happens because the timeout of the previous clicks still exists as their
 // time hasn't exhausted yet and we made another click.
+
+
+// Question - Throttle Polyfill
+
+// Answer:
+const btn = document.querySelector('.increment_btn');
+const btnPress = document.querySelector('.increment_pressed');
+const count = document.querySelector('.increment_count');
+
+var pressedCount = 0;
+var triggeredCount = 0;
+
+// // Method 1: Using inbuilt Lodash throttle function
+// const throttledCount = _.throttle(() => {
+//     count.innerHTML = ++triggeredCount;
+// }, 800);
+
+// Method 2: Using Throttle Polyfill
+// Requirements seen from the Lodash throttle function:
+// - Takes in: a callback and a time (delay)
+// - Returns: a throttled function 
+const myThrottle = (cb, delay) => {
+    let last = 0;
+
+    return function (...args) {
+        let now = new Date().getTime();
+        if (now - last < delay) return;
+        last = now;
+        return cb(...args);
+    };
+}; 
+
+const myThrottledCount = myThrottle(() => {
+    count.innerHTML = ++triggeredCount;
+}, 800);
+
+btn.addEventListener('click', () => {
+    btnPress.innerHTML = ++pressedCount;
+    myThrottledCount();
+});
