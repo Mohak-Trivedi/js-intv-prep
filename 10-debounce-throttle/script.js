d09deffaadfe3b15f84fcb71cfa6cda43d7405f6
Mohak-Trivedi <trivedimohak@gmail.com>
2024-06-18 07:51:25 +0530

    throttling intv js qs

--- 10-debounce-throttle/script.js
+++ 10-debounce-throttle/script.js
@@ -1,21 +1,47 @@
 // Debouncing and Throttling
-// Question - Craete a button UI and debounce as follows:
+// Question - Create a button UI and debounce as follows:
 // - Show "Button Pressed <X> Times" every time button is pressed.
 // - Increase "Triggered <Y> Times" count after 800ms of debounce.
 
-const btn = document.querySelector('.increment_btn');
-const btnPress = document.querySelector('.increment_pressed');
-const count = document.querySelector('.increment_count');
+// Answer:
+// const btn = document.querySelector('.increment_btn');
+// const btnPress = document.querySelector('.increment_pressed');
+// const count = document.querySelector('.increment_count');
 
-var pressedCount = 0;
-var triggeredCount = 0;
+// var pressedCount = 0;
+// var triggeredCount = 0;
 
-// Method 1: Using inbuilt Lodash debounce function
-const debouncedCount = _.debounce(() => {
-    count.innerHTML = ++triggeredCount;
-}, 800);
+// // Method 1: Using inbuilt Lodash debounce function
+// const debouncedCount = _.debounce(() => {
+//     count.innerHTML = ++triggeredCount;
+// }, 800);
 
-btn.addEventListener('click', () => {
-    btnPress.innerHTML = ++pressedCount;
-    debouncedCount();
-});
+// btn.addEventListener('click', () => {
+//     btnPress.innerHTML = ++pressedCount;
+//     debouncedCount();
+// });
+
+
+// Question - Create a button UI and throttle as follows:
+// - Show "Button Pressed <X> Times" every time button is pressed.
+// - Increase "Triggered <Y> Times" count after 800ms of debounce.
+
+// Answer:
+// const btn = document.querySelector('.increment_btn');
+// const btnPress = document.querySelector('.increment_pressed');
+// const count = document.querySelector('.increment_count');
+
+// var pressedCount = 0;
+// var triggeredCount = 0;
+
+// // Method 1: Using inbuilt Lodash throttle function
+// const throttledCount = _.throttle(() => {
+//     count.innerHTML = ++triggeredCount;
+// }, 800);
+
+// btn.addEventListener('click', () => {
+//     btnPress.innerHTML = ++pressedCount;
+//     throttledCount();
+// });
+
+
