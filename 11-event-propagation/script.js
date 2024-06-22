3531f7e6ac27984599da6601a150ddbcedc02bf8
Mohak-Trivedi <trivedimohak@gmail.com>
2024-06-22 16:45:50 +0530

    event delegation

--- 11-event-propagation/script.js
+++ 11-event-propagation/script.js
@@ -111,24 +111,54 @@ Hence, the event "propagates" and this phenomenon is called as Event Propagation
 // Answer:
 // By using event.stopPropagation() in the event from where you don't want to 
 // propagate (bubble or capture) further.
-const div = document.querySelector('div');
-const form = document.querySelector('form');
-const button = document.querySelector('button');
+// const div = document.querySelector('div');
+// const form = document.querySelector('form');
+// const button = document.querySelector('button');
 
-div.addEventListener('click', function (e) {
-    e.stopPropagation();
-    alert('div');
-});
+// div.addEventListener('click', function (e) {
+//     e.stopPropagation();
+//     alert('div');
+// });
 
-form.addEventListener('click', function (e) {
-    e.stopPropagation();
-    alert('form');
-});
+// form.addEventListener('click', function (e) {
+//     e.stopPropagation();
+//     alert('form');
+// });
 
-button.addEventListener('click', function (e) {
-    e.stopPropagation();
-    alert('button');
-});
+// button.addEventListener('click', function (e) {
+//     e.stopPropagation();
+//     alert('button');
+// });
 // Suppose we want to allow bubbling from button to form, but not further,
 // then remove e.stopPrapagation() from button's event listener, but let it be
 // in event listeners of form and further elements.
+
+
+// Question - What is Event Delegation?
+// Answer:
+// Event delegation is a technique in JavaScript where we delegate the responsibility 
+// of handling an event to a parent element. By doing so, we avoid attaching multiple 
+// event listeners to individual elements, especially when dealing with a large number 
+// of similar elements.
+// But, how an event triggered at a child is identified by the event listener 
+// defined on the parent?
+// Due to Event Bubbling.
+
+// E.g.:
+// In our case, suppose upon clicking any of the span we want to route the user
+// to that product's page. E.g clicking on span with Headphone should route us
+// to /Headphone url.
+// But, we would have to define an event listener for each event listener. This 
+// will lead to repetitive code. Also, if a new span is added dynamically, we can't
+// have an event listener for it automatically.
+// We can prevent this by event delegation. Add even listener only to the parent
+// of all the spans i.e. to the products div.
+
+// document.querySelector('.products').addEventListener('click', function (event) {
+//     console.log(event); // for checking event object properties available
+
+
+//     if(event.target.tagName === "SPAN") { // clicked on span
+//         window.location.href += "/" + event.target.className; // update url with that span product's route for routing
+//     }
+// });
