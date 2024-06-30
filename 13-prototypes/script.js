77b157575ab13303037e5fdacb125214bfb6a640
Mohak-Trivedi <trivedimohak@gmail.com>
2024-06-30 11:01:06 +0530

    prototypal inheritance

--- 13-prototypes/script.js
+++ 13-prototypes/script.js
@@ -1,4 +1,4 @@
-// What is Prototype in JS?
+// Question - What is Prototype in JS?
 // Object
 // let obj = {
 //     name: "Mohak Trivedi",
@@ -49,11 +49,11 @@
 // as "Mohak".
 
 
-// Prototype Chaining
-let person = {
-    name: "Mohak Trivedi",
-    age: 25,
-};
+// Question - Prototype Chaining
+// let person = {
+//     name: "Mohak Trivedi",
+//     age: 25,
+// };
 
 // console.log(person.alias()); // TypeError: person.alias is not a function
 // console.log(person.toString()); // [object Object]
@@ -64,22 +64,116 @@ let person = {
 // We get an error for alias() because it is neither defined in the person object
 // nor in the [[Prototype]]: Object of the person.
 
-let additional = {
-    name: "Mohak",
-    username: "Mohkya",
-    alias: function() {
-        console.log(`${this.name} is also known as ${this.username}`); 
-    }
-};
+// let additional = {
+//     name: "Mohak",
+//     username: "Mohkya",
+//     alias: function() {
+//         console.log(`${this.name} is also known as ${this.username}`); 
+//     }
+// };
 
-person.__proto__ = additional; // replacing the prototype of person with our 
+// person.__proto__ = additional; // replacing the prototype of person with our 
 // own defined object additional. NOT RECOMMENDED IN REAL WQRLD, JUST A DEMO.
 // So, the this in additional will refer to person, not additional.
 
-console.log(person.alias()); // Mohak Trivedi is also known as Mohkya
+// console.log(person.alias()); // Mohak Trivedi is also known as Mohkya
 // JS looks for alias() in person, but didn't find it so searched in its prototype,
 // i.e. additional. Found it in additional and executed it.
 // In alias(), for this.name, looked for name in person and found it as "Mohak Trivedi"
 // and for this.username, couldn't find in person, so looked in prototype i.e. 
 // additional and found it as "Mohkya", so used these values in the console log.
 // This is called as Prototype Chaining.
+
+
+// Question - [V.IMP] - Prototype Inheritance
+// Even though, we are just replacing the prototype in the above e.g., we are
+// kind of inheriting the properties of additional in person object.
+
+// Defined a Constructor function
+function Animal(name) {
+    this.name = name;
+}
+
+// type out Animal.prototype in console, you'll notice:
+// constructor and [[Prototype]]: Object
+
+// Add a method to the prototype
+Animal.prototype.sayName = function () {
+    console.log("My name is " + this.name);
+};
+
+// type out Animal.prototype in console, you'll notice:
+// sayName(), constructor and [[Prototype]]: Object
+
+// Create object animal1 of Animal
+var animal1 = new Animal("Tiger");
+// new keyword creates a new object and sets the constructor and sets the prototype
+// of Animal as the prototype of this new object created, 
+// i.e. typing animal1.__proto__ will give same result as Animal.prototype.
+// type out animal1 in console, you'll notice: Animal { name: "Tiger" }
+// Upon expanding that Animal, you'll notice: name and [[Prototype]]: Object
+// Upon expanding the Prototype, you'll notice the same output that you got
+// upon typing Animal.prototype
+
+// Up until now, we saw how to create an instance using Constructor and new keyword 
+// and how prototypes contribute in this process.
+// This is how we used to create constructors and objects before ES6.
+// JS Classes came from ES6 onwards, we will check those in the next session.
+// Now, let's see Prototypal Inheritance:
+
+function Dog(name, breed) {
+    Animal.call(this, name); // inheriting Animal, as Dog is an Animal
+    this.breed = breed; // Dog's own property apart from properties inherited 
+    // from Animal.
+}
+// type out Dog.prototype in console, you'll notice:
+// constructor and [[Prototype]]: Object
+
+// var dog1 = new Dog("Rolex", "Labrador");
+// type out dog1, you'll see:
+// Dog {name: 'Rolex', breed: 'Labrador'}
+// So, the name is getting assigned from Animal()
+// but, this doesn't mean that inheritance is complete. 
+// type out dog1.sayName(), you'll get:
+// Uncaught TypeError: dog1.sayName is not a function
+// because the properties from the prototype of Animal are yet to be inherited.
+
+Dog.prototype = Object.create(Animal.prototype); // inheriting the properties of
+// not just Animal, but also of its prototype.
+
+// var dog1 = new Dog("Rolex", "Labrador");
+// Now, if you type out dog1.sayName(), you'll see:
+// My name is Rolex
+
+// But, inheriting the prototype of Animal in Dog, will also replace the 
+// Constructor of Dog. 
+// type out Dog.prototype, you'll notice:
+// Animal {}
+// Upon expanding it, you get:
+// [[Prototype]]: Object
+// Upon expanding it, you get the same result as typing out Animal.prototype:
+// sayName(), constructor and [[Prototype]]: Object
+
+// We don't want that to happen, so need to mention it explicitly:
+Dog.prototype.constructor = Dog;
+// type out Dog.prototype, you'll notice:
+// Animal {}
+// Upon expanding it, you get:
+// constructor: ƒ Dog(name, breed)
+// [[Prototype]]: Object 
+
+// Let's add one more method to the prototype of Dog:
+Dog.prototype.bark = function() {
+    console.log("Woof!");
+};
+
+// type out Dog.prototype, you'll notice:
+// Animal {bark: ƒ}
+// Upon expanding it, you get:
+// bark: f()
+// constructor: ƒ Dog(name, breed)
+// [[Prototype]]: Object 
+
+var dog1 = new Dog("Rolex", "Labrador");
+dog1.bark();
+dog1.sayName();
