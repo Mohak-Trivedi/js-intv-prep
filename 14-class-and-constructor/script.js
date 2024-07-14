a13abb05a38c3ee63fd9a75558fd581bd4ea12d2
Mohak-Trivedi <trivedimohak@gmail.com>
2024-07-14 23:40:34 +0530

    classes and constructors JS

--- 14-class-and-constructor/script.js
+++ 14-class-and-constructor/script.js
@@ -134,3 +134,278 @@
 // const teacher1 = Teacher.paidCourse();
 // Type and check in console that an object of teacher has been created and its
 // reference is given to teacher1.
+
+////////////////////////////////////////////////////////
+
+// Interview Questions:
+
+// Question - Explain the difference between class and object in JS.
+// A class is a blueprint that defines the structure and behavior of objects.
+// Objects are instances of classes that passes properties and methods defined
+// by the class.
+// E.g.:
+// Class
+// class Teacher {
+//     constructor(name, subject) {
+//         this.name = name;
+//         this.subject = subject;
+//         this.classes = 0;
+//     }
+
+//     intro() {
+//         return `Hello students! My name is ${this.name}, and I'll teach you ${this.subject}`;
+//     }
+
+//     teach() {
+//         this.classes++;
+//     }
+// }
+
+// // Objects
+// const teacher1 = new Teacher("Ram", "History");
+// const teacher2 = new Teacher("Shyam", "Computer Science");
+// console.log(teacher1.intro());
+// console.log(teacher2.intro()); 
+
+
+// Question - Predict the Output:
+// class Rectangle {
+//     constructor(width, height) {
+//         this.width = width;
+//         this.height = height;
+//     }
+
+//     area() {
+//         return this.width * this.height;
+//     }
+// }
+
+// const square = new Rectangle(5, 5);
+// const rect = new Rectangle(4, 6);
+
+// console.log(square.area());
+// console.log(rect.area());
+
+// Answer:
+// 25
+// 24
+
+
+// Question - How does inheritance work in JS classes?
+
+// Answer:
+// Inheritance in JS classes is achieved using the extends keyword. It allows
+// a subclass (child class) to inherit properties and methods from a super class
+// (parent class).
+// E.g.:
+// class Teacher {
+//     constructor(name, subject) {
+//         this.name = name;
+//         this.subject = subject;
+//         this.classes = 0;
+//     }
+
+//     intro() {
+//         return `Hello students! My name is ${this.name}, and I'll teach you ${this.subject}`;
+//     }
+
+//     teach() {
+//         this.classes++;
+//     }
+// }
+
+// class YouTubeTeacher extends Teacher {
+//     constructor(name, subject, channel, subscribers = 0) {
+//         super(name, subject);
+//         this.channel = channel;
+//         this.subscribers = subscribers; 
+//     }
+
+//     subscribe() {
+//         this.subscribers++;
+//         return `Thanks for subscribing to my channel ${this.channel}. We have
+//          ${this.subscribers} subscribers as of now.`;
+//     }
+// }
+
+// const ytTeacher1 = new YouTubeTeacher('Mohak', 'Coding', 'MohakCoder', 70);
+// console.log(ytTeacher1.intro());
+// console.log(ytTeacher1.subscribe());
+
+
+// Question: Predict the output:
+// class Employee {
+//     constructor() {
+//         this.name = "John";
+//     }
+//     constructor() {
+//         this.age = 30;
+//     }
+// }
+
+// var employeeObject = new Employee();
+// console.log(employeeObject.name);
+
+// Answer:
+// Uncaught SyntaxError: A class may only have one constructor
+
+
+// Question: Which approach is better and why?
+// const jamesbond = {
+//     firstName: "Roadside",
+//     lastName: "Coder",
+//     getFullName: function() {
+//         return `${this.firstName} ${this.lastName}`.trim();
+//     }
+// };
+
+// jamesbond.getFullName();
+
+// // OR
+
+// class Person {
+//     constructor(firstName, lastName) {
+//         this.firstName = firstName;
+//         this.lastName = lastName;
+//     }
+// }
+
+// Person.prototype.getFullName = function() {
+//     return `${this.firstName} ${this.lastName}`.trim();
+// };
+
+// const jamesBond = new Person("Roadside", "Coder");
+// jamesBond.getFullName();
+
+// Answer:
+// The first approach is better, because in the second approach, a closure is
+// maintained for each copy of an object containing the getFullName() method,
+// whereas in the second approach, the method is registered in the prototype 
+// rather than in every single object, so it is more memory-efficient. 
+
+
+// Question: Implement this:
+// const result = calc.add(10).subtract(5).multiply(2).divide(4).getResult();
+// console.log(result);
+// We must able to invoke such functions infinitely in a chained manner.
+
+// Answer:
+// class Calculator {
+//     constructor() {
+//         this.result = 0;
+//     }
+
+//     add(num) {
+//         this.result += num;
+//         return this;
+//     }
+
+//     subtract(num) {
+//         this.result -= num;
+//         return this;
+//     }
+
+//     multiply(num) {
+//         this.result *= num;
+//         return this;
+//     }
+
+//     divide(num) {
+//         this.result /= num;
+//         return this;
+//     }
+
+//     getResult() {
+//         return this.result;
+//     }
+// }
+
+// const calc = new Calculator();
+// const result = calc.add(10).subtract(5).multiply(2).divide(4).getResult();
+// console.log(result);
+
+
+// Question: Inheritance and Polymorphism
+// Implement a `Shape` class with an `area()` method.
+// Create subclasses `Circle` and `Square` that inherit from `Shape` and override
+// the `area()` method to calculate their respective areas.
+// class Shape {
+//     area() {
+//         return 0;
+//     }
+// }
+
+// class Circle extends Shape {
+//     constructor(radius) {
+//         super();
+//         this.radius = radius;
+//     }
+
+//     area() {
+//         return Math.PI * this.radius * this.radius;
+//     }
+// }
+
+// class Square extends Shape {
+//     constructor(side) {
+//         super();
+//         this.side = side;
+//     }
+
+//     area() {
+//         return this.side ** 2;
+//     }
+// }
+
+// const circle1 = new Circle(4);
+// const square1 = new Square(2);
+// console.log(circle1.area());
+// console.log(square1.area());
+
+
+// Question: What are Getters and Setters in JS?
+
+// Answer: 
+// Getters and Setters are methods used to control access to the properties of a
+// class.
+// They're also used to add some validation to be done before getting or updating
+// a property.
+// e.g.:
+// class Teacher {
+//     constructor(name, channel) {
+//         this.name = name;
+//         this.channel = channel;
+//         this._videoLikes = 0; // _ is just a naming convention to indicate private property
+//     }
+
+//     intro() {
+//         return `Hey, it's ${this.name}! Welcome to ${this.channel}`;
+//     }
+
+//     like() {
+//         this._videoLikes++;
+//         return `Liked this video! Current Likes: ${this._videoLikes}`;
+//     }
+
+//     get videoLikes() {
+//         return this._videoLikes;
+//     }
+
+//     set videoLikes(likes) {
+//         if(likes < 0) throw new Error("Must be more than 0");
+//         else this._videoLikes = likes;
+//     }
+// }
+
+// const mohakcoder = new Teacher("Mohak", "MohakCoder");
+
+// In console, 
+// type out: mohakcoder._videoLikes
+// you get: 0
+// type out: mohakcoder._videoLikes=-1
+// you get: -1 (Instead of getting an error)
+// Now refresh the browser and in console:
+// type out: mohakcoder.videoLikes (not ._videoLikes i.e. accessing via getter)
+// you get: 0
+// type out: mohakcoder.videoLikes=-1 (not ._videoLikes i.e. accessing via setter)
+// you get: Uncaught Error: Must be more than 0
